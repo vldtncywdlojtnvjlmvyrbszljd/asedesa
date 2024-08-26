3f010873511056fb57fd6b6d6a4d2204f00394c9
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-27 01:46:27 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,7 @@
 getgenv().Games = {
-  [2753915549] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/__-.lua", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/__-.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/__-.lua", -- Blox Fruit Sea 3
+  [2753915549] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/U_-B_-F.lua", -- Bl 1
+  [4442272183] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/U_-B_-F.lua", -- Bl 2
+  [7449423635] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/U_-B_-F.lua", -- Bl 3
   [1724616059510] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/_-_s_e_-_a.lua", -- Memesea
   [8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 1
   [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
