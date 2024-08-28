1cf6d1238301681f74d57632bb3664c8c047e64d
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-28 20:41:13 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -3,6 +3,7 @@ getgenv().Games = {
   [4442272183] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/2.lua", -- Bl 2
   [7449423635] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/U_-B_-F.lua", -- Bl 3
   [10260193230] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/_-_s_e_-_a.lua", -- Memesea
+  [13772394625] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_B_B-.lua", -- Blade Ball
   [8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 1
   [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
   [17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 3
