0cd4e142d957b6a7f7b2db268fa14fdce133a72e
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-29 05:09:49 +0700

    Update _L-I_S-T.lua

--- _L-I_S-T.lua
+++ _L-I_S-T.lua
@@ -1,12 +1,15 @@
-local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
-
-NotificationLibrary:SendNotification("Warning", "Script Loading ...", 10)
-NotificationLibrary:SendNotification("Warning", "YouTube: Medusa Script Roblox", 10)
-NotificationLibrary:SendNotification("Warning", "Hello: ".. game.Players.LocalPlayer.Name, 10)
-NotificationLibrary:SendNotification("Warning", "Executor: ".. identifyexecutor(), 10)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/GameList.lua", true))()
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
-end
+getgenv().Games = {
+  [2753915549] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/1.lua", -- Bl 1
+  [4442272183] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/2.lua", -- Bl 2
+  [7449423635] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/U_-B_-F.lua", -- Bl 3
+  [10260193230] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/_-_s_e_-_a.lua", -- Memesea
+  [13772394625] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_B_B-.lua", -- Blade Ball
+  [15744137588] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-B_-s.lua", --Blade Slayer
+  [11520107397] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_S_-LP.lua", --Slap Battle 1
+  [6403373529] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_S_-LP.lua", --Slap Battle 2
+  [9015014224] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_S_-LP.lua", --Slap Battle 3
+  [8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 1
+  [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
+  [17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 3
+  [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
+}
