983edb164f51134d6c69aac89e3f9517adf1f49e
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-10-11 14:19:04 +0700

    Update _L-I_S-T.lua

--- _L-I_S-T.lua
+++ _L-I_S-T.lua
@@ -1,15 +1,1 @@
-getgenv().Games = {
-  [2753915549] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/_-B-u_-F.lua", -- Bl 1
-  [4442272183] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/_-B-u_-F.lua", -- Bl 2
-  [7449423635] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/_-B-u_-F.lua", -- Bl 3
-  [10260193230] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-s_3-a.lua", -- Memesea
-  [13772394625] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_B_B-.lua", -- Blade Ball
-  [15744137588] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-B_-s.lua", --Blade Slayer
-  [11520107397] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_S_-LP.lua", --Slap Battle 1
-  [6403373529] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_S_-LP.lua", --Slap Battle 2
-  [9015014224] = "https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/-_S_-LP.lua", --Slap Battle 3
-  [8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 1
-  [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
-  [17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 3
-  [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
-}
+
